8aa3f842f558e41483db02540f75f7657151b0e6
rbcse <139702117+rbcse@users.noreply.github.com>
2024-07-16 09:01:07 +0530

    Update authenticate.js

--- Backend/authenticate.js
+++ Backend/authenticate.js
@@ -1,15 +1,14 @@
 import User from "./usermodel.js";
 import jwt from "jsonwebtoken";
 const Authenticate = async (req, res, next) => {
-    console.log('Session:', req.session);
-    try {
-        const token = req.session.jwt; 
-        if (!token) {
+    try{
+        const token = req.headers.authorization;
+        console.log(token);
+        if(!token){
             throw new Error("No token provided");
         }
-
-        const verifyToken = jwt.verify(token, "TOP SECRET");
-        const rootUser = await User.findOne({ _id: verifyToken.userId });
+        const verifyToken = jwt.verify(token,"TOP SECRET");
+        const rootUser = await User.find({_id:verifyToken.userId});
 
         if (!rootUser) {
             throw new Error("User not found");
@@ -19,7 +18,8 @@ const Authenticate = async (req, res, next) => {
         req.rootUser = rootUser;
         req.userId = rootUser._id;
         next();
-    } catch (err) {
+    }
+    catch(err){
         res.status(401).send("Unauthorized user");
         console.log(err);
     }
